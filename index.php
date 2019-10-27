7ea313d4ec939816f6f8affe0d09ae3a2f76faa0
hamza <hamza.tas1992@gmail.com>
2019-10-27 05:48:58 -0400

    First commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head >
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Home - Hamza Tasneem</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+    <link href="public/fonts/font_quicksand.css" rel="stylesheet">
+    <link rel='stylesheet' type='text/css' media='screen' href="public/css/index_flexbox.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="nav">
+            <div class="home-col home-left-col">
+                <div class="media">
+                    <img src="public/img/personal_avatar.png" alt="Hamza Tasneem" style="width:60px;">
+                    <div class="media-body ">
+                        <h4>Hamza Tasneem <small><i></i></small></h4>
+                        <p>Software Engineer | Web Developer</p>
+                    </div>
+                </div>
+            </div>
+            <div class=" home-col home-mid-col "></div>
+            <div class=" home-col home-right-col "></div>
+
+        </div>
+        <div class="main">
+            <div class=" home-col home-left-col ">
+
+                <div class="col-content">
+                    <span><a href="https://mywork.hamzatasneem.com">My Work</a>
+                    </span>
+                    <hr>
+                    <div>Freelance</div>
+                    <div>Personal</div>
+                    <div>Team Projects</div>
+                </div>
+            </div>
+            <div class=" home-col home-mid-col ">
+                <div class="col-content">
+                    <span><a href="https://blog.hamzatasneem.com">My Blog &nbsp;</a>
+                    </span><hr>
+                    <div>Personal</div>
+                    <div>Affiliates</div>
+                    <div>Others</div>
+
+                </div>
+            </div>
+            <div class=" home-col home-right-col ">
+                <div class="col-content">
+                    <span><a href="https://hamzatasneem.com/about">My Self &nbsp;&nbsp;</a> 
+                    </span>
+                    <hr>
+                    <div>Services</div>
+                    <div>About</div>
+                    <div>Contact</div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</body>
+
+</html>
